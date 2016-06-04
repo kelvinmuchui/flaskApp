a12abdae72ad4291815da28624f2247bd7ad42fc
kelvinmuchui <muchuikelvin423@gmail.com>
2016-06-04 07:50:11 +0300

    add venv

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def main():
-    return "welcome!!"
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
